bd4d63455098eb9716f01e8a43bf82d5a8036f73
Jack Wright <jack@builtontheweb.co.uk>
2021-03-30 12:17:20 +0100

    d

--- getXML.php
+++ getXML.php
@@ -6,7 +6,7 @@
  * @date     29/13/2021
 **/
 
-$url = $_GET['URL'];
+$url = $_GET['url'];
 $xml = simplexml_load_file($url, "SimpleXMLElement", LIBXML_NOCDATA);
 $json = json_encode($xml);
 $products = json_decode($json,TRUE);
