2a7b3e6615edc23bcaaef5ac2d4dd57bcea31c1c
Jack Wright <jack@builtontheweb.co.uk>
2021-03-30 12:30:05 +0100

    Ready for mysql Import

--- getXML.php
+++ getXML.php
@@ -6,7 +6,7 @@
  * @date     29/13/2021
 **/
 
-$url = htmlspecialchars($_GET['url']);
+$url = "https://alterego-lingerie.com/wp-load.php?security_token=d61a45e35f7c71e3&export_id=235&action=get_data"
 echo $url;
 $xml = simplexml_load_file($url, "SimpleXMLElement", LIBXML_NOCDATA);
 $json = json_encode($xml);
