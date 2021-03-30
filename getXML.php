08935ccc115628ff63fdfeb40771aeb1dd9ae62f
Jack Wright <jack@builtontheweb.co.uk>
2021-03-30 12:04:02 +0100

    Commit

--- getXML.php
+++ getXML.php
@@ -6,5 +6,9 @@
  * @date     29/13/2021
 **/
 
+$url = $_GET['URL'];
+$xml = simplexml_load_file($url, "SimpleXMLElement", LIBXML_NOCDATA);
+$json = json_encode($xml);
+$products = json_decode($json,TRUE);
 
 ?>
