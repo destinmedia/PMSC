2987c3cafa9d4d91a0a3998cf2bb9250093c801b
Jack Wright <jack@builtontheweb.co.uk>
2021-04-01 01:19:40 +0100

    ss

--- getXML.php
+++ getXML.php
@@ -6,13 +6,15 @@
  * @date     29/13/2021
 **/
 
-$url = "https://alterego-lingerie.com/wp-load.php?security_token=d61a45e35f7c71e3&export_id=235&action=get_data";
+$url = "https://alterego-lingerie.com/wp-load.php?security_token=0f15c97507f1fc3c&export_id=113&action=get_data";
 echo $url;
-$xml = simplexml_load_file($url, "SimpleXMLElement", LIBXML_NOCDATA);
-$json = json_encode($xml);
-$products = json_decode($json,TRUE);
-echo "<pre>";
-print_r($products['post']);
-echo "</pre>";
+$file = fopen($url, 'r');
+while (($line = fgetcsv($file)) !== FALSE) {
+  //$line is an array of the csv elements
+  echo "<pre>";
+  print_r($line);
+  echo "</pre>";
+}
+fclose($file);
 
 ?>
